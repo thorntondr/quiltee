c4e9561c94274b4a00fe238f69364dc23653313b
Daniel Thornton <danielthornton@outlook.com>
2023-04-05 19:18:19 -0700

    Add results selection pane

--- js/results.js
+++ js/results.js
@@ -1,11 +1,11 @@
-function genAndDisplay() {
+function selectResult(result) {
   // Generate and Display pattern
-  var resultsContainer = document.getElementById("results-container");
-  resultsContainer.replaceChildren(generateAndDraw());
+  let patternContainer = document.getElementById("pattern-container");
+  patternContainer.replaceChildren(result.cloneNode(true));
 }
 
 function printResults() {
-  const content = document.getElementById("results-container").innerHTML;
+  const content = document.getElementById("pattern-container").innerHTML;
   const printWindow = window.open('', 'PrintWindow', 'height=400,width=600');
   printWindow.document.write('<html><head><title>Print Window</title>');
   printWindow.document.write('<link rel="stylesheet" type="text/css" href="style.css">');
@@ -18,8 +18,21 @@ function printResults() {
   printWindow.close();
 }
 
+function addNewPattern() {
+  // Generate and Display pattern
+  let resultsContainer = document.getElementById("results-container");
+  let outerDiv = document.createElement("div");
+  outerDiv.classList.add("result");
+  let result = generateAndDraw();
+  outerDiv.addEventListener("click", () => {selectResult(result);});
+  outerDiv.appendChild(result);
+  resultsContainer.appendChild(outerDiv);
+  return result;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  genAndDisplay();
-  document.getElementById('generate-button').addEventListener("click", genAndDisplay);
+  result = addNewPattern();
+  selectResult(result);
+  document.getElementById('generate-button').addEventListener("click", addNewPattern);
   document.getElementById('print-button').addEventListener("click", printResults);
 });
